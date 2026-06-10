e1f96af8d2b2b25419c0ae8d6d4759c344c03ca9
astudillloguadalupe <astudillloguadalupe@gmail.com>
2026-06-10 16:57:41 -0300

    foreach

---
+++ pryBaseDeDatos-Astudillo/frmConsultaClientes(Foreach).cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryBaseDeDatos_Astudillo
+{
+    public partial class frmConsultaClientes_Foreach_ : Form
+    {
+        public frmConsultaClientes_Foreach_()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmConsultaClientes_Foreach__Load(object sender, EventArgs e)
+        {
+            clsClientes x = new clsClientes();
+            x.ListarForeach(dgvConsulta);
+        }
+    }
+}
